3805038fca23081eca025799db6aeffe48773a7c
Jim Herd <jtherd@gmail.com>
2018-09-04 23:32:26 +0100

    Redesign of bus FSM

--- verilog/uP_interface.sv
+++ verilog/uP_interface.sv
@@ -16,9 +16,17 @@ module uP_interface(
                    );
                    
 
-logic counter_zero, set_in_byte_count, set_out_byte_count, increment_count, read_byte, write_byte;
+logic counter_zero, set_in_uP_byte_count, set_out_uP_byte_count, read_uP_byte, write_uP_byte;
 
-logic ack, start, handshake2_1, handshake2_2;
+logic handshake2_1, handshake2_2;
+logic  read_bus_word, clear_uP_packet;
+logic  set_in_bus_word_count, set_out_bus_word_count;
+
+
+//logic  uP_handshake_1, uP_start, uP_soft_reset;
+//logic  uP_handshake_2, uP_ack;
+
+
 
 byte_t counter, target_count;
 
@@ -29,19 +37,21 @@ byte_t output_packet[`NOS_WRITE_BYTES];
 uP_interface_FSM uP_interface_sys(
                .clk(clk), 
                .reset(reset), 
-               .word_RW(bus.RW), 
-               .word_handshake_1(bus.handshake1_1),
-               .word_handshake_2(bus.handshake1_2),
-               .packet_start(uP_start), 
-               .packet_ack(uP_ack),
-               .soft_reset(uP_soft_reset),
-               .byte_handshake_1(uP_handshake_1),
-               .byte_handshake_2(uP_handshake_2),
+               .bus_RW(bus.RW), 
+               .bus_handshake_1(bus.handshake1_1),
+               .bus_handshake_2(bus.handshake1_2),
+               .uP_start(uP_start), 
+               .uP_ack(uP_ack),
+               .uP_soft_reset(uP_soft_reset),
+               .uP_handshake_1(uP_handshake_1),
+               .uP_handshake_2(uP_handshake_2),
                .counter_zero(counter_zero),
-               .set_in_byte_count(set_in_byte_count), 
-               .set_out_byte_count(set_out_byte_count), 
-               .read_byte(read_byte), 
-               .write_byte(write_byte)
+               .set_in_uP_byte_count(set_in_uP_byte_count), 
+               .set_out_uP_byte_count(set_out_uP_byte_count),
+               .set_out_bus_word_count(set_out_bus_word_count),
+               .read_uP_byte(read_uP_byte), 
+               .write_uP_byte(write_uP_byte),
+               .clear_uP_packet(clear_uP_packet)
                ); 
 
 
@@ -51,22 +61,22 @@ always_ff @(posedge clk) begin
       target_count <= 0;;
    end 
    else begin
-      if (set_in_byte_count == 1'b1) begin
+      if (set_in_uP_byte_count == 1'b1) begin
          target_count <= `NOS_READ_BYTES;
          counter <= 0;
       end else begin
-         if (set_out_byte_count == 1'b1) begin
+         if (set_out_uP_byte_count == 1'b1) begin
             target_count <= `NOS_WRITE_BYTES;
             counter <= 0;
          end
          else begin  
-            if (read_byte == 1'b1) begin
+            if (read_uP_byte == 1'b1) begin
                input_packet[counter] <= uP_data_out;
                counter <= counter + 1'b1;
                target_count <= target_count - 1'b1;
             end
             else begin
-               if (write_byte == 1'b1) begin
+               if (write_uP_byte == 1'b1) begin
                   uP_data_in <= output_packet[0];
                   counter <= counter + 1'b1;
                   target_count <= target_count - 1'b1;
@@ -84,6 +94,5 @@ assign  counter_zero = (counter == 0) ? 1'b1 : 1'b0;
 assign  bus.reg_address = input_packet[1];
 assign  bus.data_out = {input_packet[5],input_packet[4],input_packet[3],input_packet[2]};
 
-assign 
               
 endmodule
--- verilog/uP_interface_FSM.sv
+++ verilog/uP_interface_FSM.sv
@@ -1,4 +1,4 @@
-//
+ //
 // uP_interface_FSM.sv : 
 //
 // State machine to run 32-bit interface. Moore machine
@@ -6,23 +6,30 @@
 `include  "global_constants.sv"
 
 module uP_interface_FSM(
-               input  logic  clk, reset, 
-               output logic  word_RW, word_handshake_1,
-               input  logic  word_handshake_2,
-               input  logic  byte_handshake_1, packet_start, soft_reset,
-               output logic  byte_handshake_2, packet_ack,
-               output logic  read_byte, write_byte,               
-               input  logic  counter_zero,
-               output logic  set_in_byte_count, set_out_byte_count
-               );
-
-enum bit [5:0] {	S_M0, S_M1,
-                    S_R0, S_R1, S_R2, S_R3,
-                  S_M2, S_M3,
-                    S_E0, S_E1, 
-                  S_M4, 
-                    S_W0, S_W1, S_W2, S_W3, S_W4,
-                  S_M5, S_M6, S_M7, S_M8
+            input  logic  clk, reset, 
+            output logic  bus_RW, bus_handshake_1,
+            input  logic  bus_handshake_2,
+            input  logic  uP_handshake_1, uP_start, uP_soft_reset, counter_zero,
+            output logic  uP_handshake_2, uP_ack,
+            output logic  read_uP_byte, write_uP_byte, read_bus_word, clear_uP_packet,             
+            output logic  set_in_uP_byte_count, set_out_uP_byte_count,
+            output logic  set_in_bus_word_count, set_out_bus_word_count
+            );
+//
+// set of FSM states
+//
+enum bit [6:0] {	// section #1
+                     S_M0, 
+                     S_RuP0, S_RuP1, S_RuP2, S_RuP3, S_RuP4, S_RuP5,
+                  // section #2
+                     S_WB0, S_WB1, S_WB2, S_WB3,
+                  // section #3
+                     S_RB0, S_RB1, S_RB2, S_RB3, S_RB4, S_RB5, 
+                  // section #4 
+                     S_WuP0, S_WuP1, S_WuP2, S_WuP3, S_WuP4, S_WuP5, S_WuP6,
+                  // section #5 
+                     S_M1, S_M2,
+                     S_M3, S_M4, S_M5 
 					} state, next_state;
 	
 
@@ -37,114 +44,238 @@ always_comb begin: set_next_state
 	next_state = state;	// default condition is next state is present state
 	unique case (state)
 		S_M0:
-			next_state = (packet_start) ? S_M1 : S_M2;   
-      S_M1: 
-         next_state = S_R0;
-         
-      S_R0 :  // read a byte from uP 
-         next_state = (byte_handshake_1 == 1) ? S_R1 : S_R0; 
-      S_R1 :
-         next_state = S_R2;
-      S_R2 :
-         next_state = S_R3;
-      S_R3 : 
-         next_state = (byte_handshake_1 == 0) ? S_M2 : S_R3; 
-         
-      S_M2:
-			next_state = S_M3;
+			next_state = (uP_start) ? S_M1 : S_M2;
+      //   
+      // read a byte packet from uP and check for "soft_reset" command
+      //    
+      S_RuP0 :
+         next_state = S_RuP1;
+      S_RuP1 :
+         next_state = (uP_handshake_1 == 1) ? S_RuP2 : S_RuP1; 
+      S_RuP2 :
+         next_state = S_RuP3;
+      S_RuP3 :
+         next_state = S_RuP4;
+      S_RuP4 : 
+         next_state = (uP_handshake_1 == 0) ? S_RuP5 : S_RuP4; 
+      S_RuP5 :
+         next_state = (counter_zero == 0) ? S_WB0 : S_M1;
+      S_M1   :
+         next_state = (uP_soft_reset == 0) ? S_WB0 : S_M2;
+      S_M2   :
+         next_state = S_WuP0;
+      //
+      // send data onto system 32-bit bus
+      //
+      S_WB0 :
+         next_state = S_WB1;
+      S_WB1 :
+         next_state = (bus_handshake_1 == 1) ? S_WB2 : S_WB1; 
+      S_WB2 :
+         next_state = S_WB3;
+      S_WB3 : 
+         next_state = (bus_handshake_1 == 0) ? S_RB0 : S_WB3; 
+      //
+      // read data from addressed slave
+      //
+      S_RB0 :
+         next_state = S_RB1;
+      S_RB1 :
+         next_state = S_RB2;   
+      S_RB2 :
+         next_state = (bus_handshake_1 == 1) ? S_RB3 : S_RB2; 
+      S_RB3 :
+         next_state = S_RB4;
+      S_RB4 : 
+         next_state = (bus_handshake_1 == 0) ? S_RB5 : S_RB4; 
+      S_RB5 :
+         next_state = (counter_zero == 0) ? S_WuP0 : S_RB1;   
+      //
+      // send data packet to uP
+      //
+      S_WuP0:  
+         next_state = S_WuP1;
+      S_WuP1:
+         next_state = S_WuP2;
+      S_WuP2:
+         next_state = S_WuP3;
+      S_WuP3: 
+         next_state = (uP_handshake_1 == 1) ? S_WuP4 : S_WuP3; 
+      S_WuP4:
+         next_state = S_WuP5;
+      S_WuP5: 
+         next_state = (uP_handshake_1 == 0) ? S_WuP6 : S_WuP5; 
+      S_WuP6 :
+         next_state = (counter_zero == 0) ? S_M3 : S_WuP1;  
+      //
+      // complete data transaction with uP
+      //
       S_M3:
-			next_state = (counter_zero == 0) ? S_E0 : S_R0; 
+			next_state = S_M4;
       S_M4:
-			next_state = S_W0; 
-         
-      S_W0:  // write a byte to uP 
-         next_state = S_W1;
-      S_W1:
-         next_state = S_W2;
-      S_W2: 
-         next_state = (byte_handshake_1 == 1) ? S_W3 : S_W2; 
-      S_W3:
-         next_state = S_W4;
-      S_W4: 
-         next_state = (byte_handshake_1 == 0) ? S_M5 : S_W4; 
-      
+			next_state = (uP_start == 0) ? S_M5 : S_M4; 
       S_M5:
-			next_state = S_M6;
-      S_M6:
-			next_state = (counter_zero == 0) ? S_M7 : S_W0; 
-      S_M7:
-			next_state = S_M8; 
-		S_M8:
-			next_state = (packet_start) ? S_M8 : S_M0;     
-		default:
-			next_state = S_M0;
+			next_state = S_M1; 
 	endcase
 end: set_next_state
 
 always_comb  begin: set_outputs
 	case (state)
-		S_M0, S_M3, S_M6, S_M8, S_R0, S_R3, S_W0, S_W2, S_W3, S_W4 : begin
-            word_RW = 0;
-            word_handshake_1     = 0;
-            byte_handshake_2     = 0;
-            set_in_byte_count    = 0;
-            set_out_byte_count   = 0;
-            read_byte            = 0; 
-            write_byte           = 0;
+		S_M0, S_RuP1, S_RuP5, S_WB2, S_WB3, S_RB4, S_RB5, S_WuP4, S_WuP5, S_WuP6, S_M1, S_M5  : begin
+            bus_handshake_1         = 0;
+            uP_handshake_2          = 0;
+            uP_ack                  = 0;
+            set_in_uP_byte_count    = 0;
+            set_out_uP_byte_count   = 0;
+            set_in_bus_word_count   = 0;
+            read_uP_byte            = 0; 
+            write_uP_byte           = 0;
+            read_bus_word           = 0;
+            clear_uP_packet         = 0;
          end
-		S_M1 : begin
-            word_RW = 0;
-            word_handshake_1     = 0;
-            byte_handshake_2     = 0;
-            set_in_byte_count    = 1;
-            set_out_byte_count   = 0;
-            read_byte            = 0; 
-            write_byte           = 0;
+		S_RuP0 : begin
+            bus_handshake_1         = 0;
+            uP_handshake_2          = 0;
+            uP_ack                  = 0;
+            set_in_uP_byte_count    = 1;
+            set_out_uP_byte_count   = 0;
+            set_in_bus_word_count   = 0;
+            read_uP_byte            = 0; 
+            write_uP_byte           = 0;
+            read_bus_word           = 0;
+            clear_uP_packet         = 0;
          end
-      S_M2, S_M5 : begin
-            word_RW = 0;
-            word_handshake_1     = 0;
-            byte_handshake_2     = 0;
-            set_in_byte_count    = 0;
-            set_out_byte_count   = 0;
-            read_byte            = 0; 
-            write_byte           = 0;
+      S_RuP2 : begin
+            bus_handshake_1         = 0;
+            uP_handshake_2          = 0;
+            uP_ack                  = 0;
+            set_in_uP_byte_count    = 0;
+            set_out_uP_byte_count   = 0;
+            set_in_bus_word_count   = 0;
+            read_uP_byte            = 1; 
+            write_uP_byte           = 0;
+            read_bus_word           = 0;
+            clear_uP_packet         = 0;
          end
-      S_M4 : begin
-            word_RW = 0;
-            word_handshake_1     = 0;
-            byte_handshake_2     = 0;
-            set_in_byte_count    = 0;
-            set_out_byte_count   = 1;
-            read_byte            = 0; 
-            write_byte           = 0;
+      S_RuP3, S_RuP4  : begin
+            bus_handshake_1         = 0;
+            uP_handshake_2          = 1;
+            uP_ack                  = 0;
+            set_in_uP_byte_count    = 0;
+            set_out_uP_byte_count   = 0;
+            set_in_bus_word_count   = 0;
+            read_uP_byte            = 0; 
+            write_uP_byte           = 0;
+            read_bus_word           = 0;
+            clear_uP_packet         = 0;
          end
-      S_R1 : begin
-            word_RW = 0;
-            word_handshake_1     = 0;
-            byte_handshake_2     = 0;
-            set_in_byte_count    = 0;
-            set_out_byte_count   = 0;
-            read_byte            = 1; 
-            write_byte           = 0;
+      S_WB0, S_WB1 : begin
+            bus_handshake_1         = 1;
+            uP_handshake_2          = 0;
+            uP_ack                  = 0;
+            set_in_uP_byte_count    = 0;
+            set_out_uP_byte_count   = 0;
+            set_in_bus_word_count   = 0;
+            read_uP_byte            = 0; 
+            write_uP_byte           = 0;
+            read_bus_word           = 0;
+            clear_uP_packet         = 0;
          end
-      S_R2, S_W1 : begin
-            word_RW = 0;
-            word_handshake_1     = 0;
-            byte_handshake_2     = 1;
-            set_in_byte_count    = 0;
-            set_out_byte_count   = 0;
-            read_byte            = 0; 
-            write_byte           = 0;
+      S_RB0 : begin
+            bus_handshake_1         = 0;
+            uP_handshake_2          = 0;
+            uP_ack                  = 0;
+            set_in_uP_byte_count    = 1;
+            set_out_uP_byte_count   = 0;
+            set_in_bus_word_count   = 0;
+            read_uP_byte            = 0; 
+            write_uP_byte           = 0;
+            read_bus_word           = 0;
+            clear_uP_packet         = 0;
+         end
+      S_RB1, S_RB2 : begin
+            bus_handshake_1         = 1;
+            uP_handshake_2          = 0;
+            uP_ack                  = 0;
+            set_in_uP_byte_count    = 0;
+            set_out_uP_byte_count   = 0;
+            set_in_bus_word_count   = 0;
+            read_uP_byte            = 0; 
+            write_uP_byte           = 0;
+            read_bus_word           = 0;
+            clear_uP_packet         = 0;
          end   
+      S_RB3 : begin
+            bus_handshake_1         = 0;
+            uP_handshake_2          = 0;
+            uP_ack                  = 0;
+            set_in_uP_byte_count    = 0;
+            set_out_uP_byte_count   = 0;
+            set_in_bus_word_count   = 0;
+            read_uP_byte            = 0; 
+            write_uP_byte           = 0;
+            read_bus_word           = 1;
+            clear_uP_packet         = 0;
+         end 
+      S_WuP0 : begin
+            bus_handshake_1         = 0;
+            uP_handshake_2          = 0;
+            uP_ack                  = 0;
+            set_in_uP_byte_count    = 0;
+            set_out_uP_byte_count   = 1;
+            set_in_bus_word_count   = 0;
+            read_uP_byte            = 0; 
+            write_uP_byte           = 0;
+            read_bus_word           = 0;
+            clear_uP_packet         = 0;
+         end 
+      S_WuP1,  S_WuP3 : begin
+            bus_handshake_1         = 0;
+            uP_handshake_2          = 0;
+            uP_ack                  = 0;
+            set_in_uP_byte_count    = 0;
+            set_out_uP_byte_count   = 0;
+            set_in_bus_word_count   = 0;
+            read_uP_byte            = 0; 
+            write_uP_byte           = 1;
+            read_bus_word           = 0;
+            clear_uP_packet         = 0;
+         end
+      S_M2 : begin
+            bus_handshake_1         = 0;
+            uP_handshake_2          = 0;
+            uP_ack                  = 0;
+            set_in_uP_byte_count    = 0;
+            set_out_uP_byte_count   = 0;
+            set_in_bus_word_count   = 0;
+            read_uP_byte            = 0; 
+            write_uP_byte           = 0;
+            read_bus_word           = 0;
+            clear_uP_packet         = 1;
+         end        
+      S_M3, S_M4 : begin
+            bus_handshake_1         = 0;
+            uP_handshake_2          = 0;
+            uP_ack                  = 1;
+            set_in_uP_byte_count    = 0;
+            set_out_uP_byte_count   = 0;
+            set_in_bus_word_count   = 0;
+            read_uP_byte            = 0; 
+            write_uP_byte           = 0;
+            read_bus_word           = 0;
+            clear_uP_packet         = 0;
+         end        
 		default: begin
-            word_RW = 0;
-            word_handshake_1     = 0;
-            byte_handshake_2     = 0;
-            set_in_byte_count    = 0;
-            set_out_byte_count   = 0; 
-            read_byte            = 0; 
-            write_byte           = 0;
+            bus_handshake_1         = 0;
+            uP_handshake_2          = 0;
+            uP_ack                  = 0;
+            set_in_uP_byte_count    = 0;
+            set_out_uP_byte_count   = 0;
+            set_in_bus_word_count   = 0;
+            read_uP_byte            = 0; 
+            write_uP_byte           = 0;
+            read_bus_word           = 0;
+            clear_uP_packet         = 0;
          end
 	endcase	
 end: set_outputs
